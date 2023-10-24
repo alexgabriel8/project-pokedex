a790a09456362b4a562af1601d40e2efc5ac9d22
alexgabriel8 <alexgabriels8@outlook.com>
2023-10-24 06:20:46 -0300

    feat: create isTypeTTheme function

--- src/constants/themes/themes.types.ts
+++ src/constants/themes/themes.types.ts
@@ -1,3 +1,5 @@
+import { themes } from "./themes";
+
 type AcceptedThemes = "light" | "dark";
 
 type TTheme = {
@@ -30,4 +32,54 @@ type TThemes = {
   [key in AcceptedThemes]: TTheme;
 };
 
+function isTypeTTheme(object: any): object is TTheme {
+  if (typeof object !== "object" || !object) return false;
+
+  function bothHaveSameProperties(
+    objectKeys: any,
+    expectedKeys: Array<string>,
+  ) {
+    if (expectedKeys.length !== objectKeys.length) return false;
+
+    for (const key in expectedKeys)
+      if (!objectKeys[key] || typeof objectKeys[key] !== "string") return false;
+
+    return true;
+  }
+
+  if (!bothHaveSameProperties(Object.keys(object), Object.keys(themes.light)))
+    return false;
+  if (
+    !bothHaveSameProperties(
+      Object.keys(object.status),
+      Object.keys(themes.light.status),
+    )
+  )
+    return false;
+  if (
+    !bothHaveSameProperties(
+      Object.keys(object.modalTogglerButton),
+      Object.keys(themes.light.modalTogglerButton),
+    )
+  )
+    return false;
+  if (
+    !bothHaveSameProperties(
+      Object.keys(object.modalTogglerButton.enabled),
+      Object.keys(themes.light.modalTogglerButton.enabled),
+    )
+  )
+    return false;
+  if (
+    !bothHaveSameProperties(
+      Object.keys(object.modalTogglerButton.disabled),
+      Object.keys(themes.light.modalTogglerButton.disabled),
+    )
+  )
+    return false;
+
+  return true;
+}
+
 export type { AcceptedThemes, TTheme, TThemes };
+export { isTypeTTheme };
