ec124c96eb08c31c4917eef4df1b1c6183c6327c
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-21 09:30:39 -0300

    fix(restructurePkmObjectSpecies.ts): deal with null habitat

--- src/services/getPokemon/restructurePkmObjectSpecies.ts
+++ src/services/getPokemon/restructurePkmObjectSpecies.ts
@@ -4,7 +4,7 @@ function restructurePkmObjectSpecies(pokemon: IPokemon, species: any) {
   pokemon.speciesInfo = {} as unknown as SpeciesInfo;
 
   pokemon.speciesInfo.generation = species.generation.name;
-  pokemon.speciesInfo.habitat = species.habitat.name;
+  pokemon.speciesInfo.habitat = species.habitat?.name ?? null;
 
   for (let i = 0; i < species.flavor_text_entries.length; i++) {
     if (species.flavor_text_entries[i].language.name === "en") {
