95f3892c0aa4575cb1a4eeef8d7ce791f606b6a2
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-27 20:58:27 -0300

    feat(getPkmAbilityDescription.ts): return error on 404 response

--- src/services/getPkmAbilityDescription/getPkmAbilityDescription.ts
+++ src/services/getPkmAbilityDescription/getPkmAbilityDescription.ts
@@ -1,12 +1,14 @@
 async function getPkmAbilityDescription(url: string) {
     const response = await fetch(url);
+    if(response.status === 404) throw new Error(`Ability from URL "${url}" not found`);
+
     const json = await response.json();
 
     if(json.effect_entries.length === 0) return "No description available."
 
     for (let i = 0; i < json.effect_entries.length; i++) {
         if(json.effect_entries[i].language.name === "en") return json.effect_entries[i].effect
-    }
+    }        
 }
 
 export { getPkmAbilityDescription }
