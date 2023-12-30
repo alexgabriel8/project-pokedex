7e7e3875f82e9fe2a80a46de15bd96b5a3833fe8
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-30 19:19:20 -0300

    feat: create Pokémon move description fetcher

---
+++ src/services/getPkmMoveDescription.ts
@@ -0,0 +1,17 @@
+async function getPkmMoveDescription(url: string) {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    let moveDescription = "No description available.";
+
+    for(let i = 0; i < json.effect_entries.length; i++) {
+        if(json.effect_entries[i].language.name === "en") {
+            moveDescription = json.effect_entries[i].effect
+            i = json.effect_entries.length
+        }
+    }
+
+    return moveDescription
+}
+
+export { getPkmMoveDescription }
