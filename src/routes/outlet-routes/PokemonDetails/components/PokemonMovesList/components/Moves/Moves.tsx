8bdd3ae98b3a29132f38e8c02c8df727b6da1d6e
alexgabriel8 <alexgabriels8@outlook.com>
2024-01-04 09:32:29 -0300

    feat(Moves.tsx): shows "No moves available" when 0 moves are passed

--- src/routes/outlet-routes/PokemonDetails/components/PokemonMovesList/components/Moves/Moves.tsx
+++ src/routes/outlet-routes/PokemonDetails/components/PokemonMovesList/components/Moves/Moves.tsx
@@ -1,19 +1,32 @@
 // Components
 import * as S from "./Moves.styles"
 import { Move } from "./Move/Move"
+import { MoveContainer } from "./Move/Move.styles"
 
 // Types
 import * as PkmTypes from "../../../../../../../types/pokemon.types"
+import { useContext } from "react"
+import { ThemeContext } from "../../../../../../../context/ThemeContext/ThemeContext"
 
 const Moves = ({ moves }: {moves: PkmTypes.Move[]}) => {
+    const { activeTheme } = useContext(ThemeContext)!;
+
     return (
-        <S.MovesContainer id="pokemon-moves">
-            {
-                moves.map((move) => (
-                    <Move key={move.name} move={move} />
-                ))
-            }
-        </S.MovesContainer>
+        moves.length === 0 ? (
+            <S.MovesContainer>
+                <MoveContainer theme={activeTheme} className="clicked">
+                    <h3 className="move-name">No moves available</h3>
+                </MoveContainer>
+            </S.MovesContainer>
+        ) : (
+            <S.MovesContainer id="pokemon-moves">
+                {
+                    moves.map((move) => (
+                        <Move key={move.name} move={move} />
+                    ))
+                }
+            </S.MovesContainer>
+        )
     )
 }
 
