c02eae5a1ea903087032b4d71a7c1c038132320d
alexgabriel8 <alexgabriels8@outlook.com>
2024-01-15 07:42:10 -0300

    refactor(SettingsOption.styles.tsx): move Settings Toggle Button to his own component

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+import { smartTv } from "../constants/breakpoints";
+
+const ToggleButton = styled.div`
+    position: relative;
+
+    width: 50px;
+    height: 20px;
+    @media (min-width: ${smartTv}px) {
+        width: 100px;
+        height: 40px;
+    }
+
+    border-radius: 20px;
+
+    &&::before {
+        display: block;
+        content: "";
+        
+        position: absolute;
+        transform: translate(-5px, -4px);
+        
+        width: 25px;
+        height: 25px;
+        @media (min-width: ${smartTv}px) {
+            width: 50px;
+            height: 50px;
+            transform: translate(-10px, -8px);
+        }
+
+        border-radius: 50%;
+
+        transition: transform 0.2s;
+    }
+
+    &&.active::before {
+        transform: translate(100%, -4px);
+        @media (min-width: ${smartTv}px) {
+            transform: translate(100%, -8px);
+        }
+    }
+
+    transition: background-color 0.25s;
+`
+
+export { ToggleButton }
--- src/routes/Root/PokemonList/PkmListBottomBar/SettingsButton/SettingsModal/SettingsOptions/SettingsOption/SettingsOption.styles.tsx
+++ src/routes/Root/PokemonList/PkmListBottomBar/SettingsButton/SettingsModal/SettingsOptions/SettingsOption/SettingsOption.styles.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+// Components
+import { ToggleButton } from "../../../../../../../../components/ToggleButton";
+
 // Breakpoints
 import { smartTv } from "../../../../../../../../constants/breakpoints";
 
@@ -43,57 +46,21 @@ const SettingsOptionContainer = styled.li`
 type TogglerIconProps = {
     theme: TTheme;
 }
-const SettingsOptionTogglerIcon = styled.div<TogglerIconProps>`
-    position: relative;
-
+const SettingsOptionTogglerIcon = styled(ToggleButton)<TogglerIconProps>`
     flex-shrink: 0;
-
-    width: 50px;
-    height: 20px;
-    @media (min-width: ${smartTv}px) {
-        width: 100px;
-        height: 40px;
-    }
 
     background-color: ${({ theme }) => theme.modalTogglerButton.backgroundDisabled};
     &&.active {
         background-color: ${({ theme }) => theme.modalTogglerButton.backgroundEnabled};
     }
-    
+
     border: 1px solid ${({ theme }) => theme.modalTogglerButton.circleBorder};
-    border-radius: 20px;
 
     &&::before {
-        display: block;
-        content: "";
-        
-        position: absolute;
-        transform: translate(-5px, -4px);
-        
-        width: 25px;
-        height: 25px;
-        @media (min-width: ${smartTv}px) {
-            width: 50px;
-            height: 50px;
-            transform: translate(-10px, -8px);
-        }
-
         background-color: ${({ theme }) => theme.modalTogglerButton.circleBackground};
 
         border: 1px solid ${({ theme }) => theme.modalTogglerButton.circleBorder};
-        border-radius: 50%;
-
-        transition: transform 0.2s;
     }
-
-    &&.active::before {
-        transform: translate(100%, -4px);
-        @media (min-width: ${smartTv}px) {
-            transform: translate(100%, -8px);
-        }
-    }
-
-    transition: background-color 0.25s;
 `
 
 export { SettingsOptionContainer, SettingsOptionTogglerIcon };
