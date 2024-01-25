42587aad65161301eff6c61a575c112a10d4d754
alexgabriel8 <alexgabriels8@outlook.com>
2024-01-25 09:19:51 -0300

    refactor(slugifyString): improve readability + fix bugs

--- src/scripts/slugifyString.ts
+++ src/scripts/slugifyString.ts
@@ -1,11 +1,9 @@
-function slugifyString(rawString: string) {
-  let processedString = rawString.trim();
-  processedString = processedString.toLowerCase();
-  processedString = processedString
-    .normalize("NFD")
-    .replace(/([\u0300-\u036f]|[^\s0-9a-zA-Z\-])/g, "");
-
-  return processedString.replace(/ /g, "-");
+function slugifyString(string: string): string {
+  return string
+    .normalize("NFD").replace(/([\u0300-\u036f]|[^\s0-9a-zA-Z])/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-");
 }
 
 export { slugifyString };
