c45644c645329ea57800a8732dd23df4df7d9f32
alexgabriel8 <alexgabriels8@outlook.com>
2024-02-10 11:05:10 -0300

    fix(vite.config.ts): configure import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
+
+const baseUrl = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": baseUrl,
+      "@components": path.resolve(baseUrl, "components"),
+      "@constants": path.resolve(baseUrl, "constants"),
+      "@context": path.resolve(baseUrl, "context"),
+      "@routes": path.resolve(baseUrl, "routes"),
+      "@scripts": path.resolve(baseUrl, "scripts"),
+      "@services": path.resolve(baseUrl, "services"),
+      "@styles": path.resolve(baseUrl, "styles"),
+      "@typings": path.resolve(baseUrl, "types"),
+      "@tests": path.resolve(baseUrl, "..", "tests"),
+    },
+  },
+  
   test: {
     environment: 'jsdom'
   },
